8cf37b58b50b6c0e754a6b587eb72ddde8196f3d
Danik499 <danikshcherban499@gmail.com>
2026-02-18 14:41:53 +0200

    Updates

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -3,7 +3,7 @@ const currentImports = {};
       let moduleMap = {
 "./Remote":()=>{
       dynamicLoadingCss(["style-D1EHTbMk.css"], false, './Remote');
-      return __federation_import('/remote-app/assets/__federation_expose_Remote-CrGBxZZx.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      return __federation_import('/remote-app/assets/__federation_expose_Remote-Bmoxktd3.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
